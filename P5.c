74ead19a20945cccdf0a2dd8425ddcd10ab44b2d
Sreepranad Devarakonda <159219231+Saucepanman-02@users.noreply.github.com>
2026-08-31 23:11:50 +0530

    Create P5.c

---
+++ P5.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    int d0, d1, d2, d3, d4, d5, d6, d7, d8, d9, d10;
+    printf("Enter first 11 digits of UPC: ");
+    scanf("%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d", &d0, &d1, &d2, &d3, &d4, &d5, &d6, &d7, &d8, &d9, &d10);
+    printf("Check digit: %d\n", 9-((d0+d2+d4+d6+d8+d10)*3+(d1+d3+d5+d7+d9)-1)%10);
+    return 0;
+}
