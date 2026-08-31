5150452a4c9f0f1d0b2395ecf9db22e2a45ae9b5
Sreepranad Devarakonda <159219231+Saucepanman-02@users.noreply.github.com>
2026-08-31 23:01:52 +0530

    Create P3.c

---
+++ P3.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    printf("Enter a 3-digit num: ");
+    int a, b, c; scanf("%1d%1d%1d", &a, &b, &c);
+    printf("Reverse: ");
+    printf("%1d%1d%1d\n", c, b, a);
+    return 0;
+}
